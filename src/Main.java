52859101efda7f0d22971c320105feee2e3dbc3a
Vladislav Pavlov <atobytes@mail.ru>
2023-01-23 00:27:33 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int deposit = 1100;
+        int bonus = (deposit / 100);
+        System.out.println((bonus+deposit+balance)+(" Ваш Баланс"));
+        System.out.println((bonus)+(" Бонусные Рубли"));
+
+
+    }
+}
