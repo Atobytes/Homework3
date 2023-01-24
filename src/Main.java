536e0d9a64fca2069aec089b372fcebe535fc3ba
Vladislav Pavlov <atobytes@mail.ru>
2023-01-25 01:46:52 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100;
         int deposit = 1100;
-        int bonus = (deposit / 100);
-        System.out.println((bonus+deposit+balance)+(" Ваш Баланс"));
-        System.out.println((bonus)+(" Бонусные Рубли"));
+        int bonus;
+        if (deposit > 100) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        int allBalance = balance + deposit + bonus;
+        System.out.println((allBalance) + (" Ваш Баланс"));
+        System.out.println((bonus) + (" Бонусные Рубли"));
 
 
     }
